4304ddf6e13f6b792e0cbf5ad8a063c94a3fa4f7
Unchaptered <workstation19961002@gmail.com>
2022-06-02 13:43:47 +0900

    ADD constant.loader.ts with constants
    Constants using for DI & IOC
    
    1. modules/filter.symlbol.ts
    2. modules/guard.symbol.ts
    3. modules/joi.symlbol.ts
    4. modules/modules/symlbol.ts
    5. routes/path.enum.ts

---
+++ src/constants/constant.loader.ts
@@ -0,0 +1,22 @@
+import { I_FILTERS, FILTERS } from './modules/filter.symbol';
+import { I_GUARDS, GUARDS } from './modules/guard.symbol';
+import { I_MODULES, MODULES } from './modules/modules.symbol';
+import { I_JOY, JOYS } from './modules/joi.symbol';
+
+import { PATHS } from './routes/paths.enum';
+
+
+export {
+    I_FILTERS,
+    FILTERS,
+
+    I_GUARDS,
+    GUARDS,
+
+    I_JOY,
+    JOYS,
+
+    I_MODULES,
+    MODULES,
+    PATHS
+};
